64dad2218fd0d5a0ae0feec31f160521c4965507
System Administrator <root@Muppalanenis-MacBook-Air.local>
2016-11-11 15:36:46 +0530

    added pusher script.

---
+++ pusher/server.js
@@ -0,0 +1,12 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+io.on('connection', function (socket) {
+  socket.emit('betstream', [['shubham', '0.01', 'lost'], ['teja', '0.02', 'won']);
+  socket.on('pot', function (data) {
+    console.log(data);
+  });
+});
--- utility.py
+++ utility.py
@@ -5,3 +5,10 @@ nums = [x for x in range(1, 11)]
 def randomdigits():
 	random.shuffle(nums)
 	return nums[:3]
+
+import redis
+
+def connect():
+	# pop satoshi from queue
+	
+	# else enqueue
