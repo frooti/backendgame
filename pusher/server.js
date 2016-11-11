339e85ce4230c85170f5766b222db115887d02a0
System Administrator <root@Muppalanenis-MacBook-Air.local>
2016-11-11 23:19:01 +0530

    added redis session.

--- pusher/server.js
+++ pusher/server.js
@@ -2,11 +2,26 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+var options = {
+	'host': 'satoshidigits-redis.sjvvfh.0001.euw1.cache.amazonaws.com',
+	'port': 6379,
+	'ttl': 3600,
+	'prefix': 'sess::',
+}
+
+app.use(session({
+    store: new RedisStore(options),
+    secret: '$at0shiN@kam0to'
+}));
+
 server.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
-server.get('/', function (req, res) {
+app.get('/', function (req, res) {
   console.log(req.session);
 });
 
@@ -16,4 +31,3 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
 });
-
