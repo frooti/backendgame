ab5e63736c50a03b3fd1de295c56f26123571880
System Administrator <root@Muppalanenis-MacBook-Air.local>
2016-11-11 15:43:00 +0530

    fixed syntax.

--- pusher/server.js
+++ pusher/server.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.listen(8080);
 
 io.on('connection', function (socket) {
-  socket.emit('betstream', [['shubham', '0.01', 'lost'], ['teja', '0.02', 'won']);
+  socket.emit('betstream', [['shubham', '0.01', 'lost'], ['teja', '0.02', 'won']]);
   socket.on('pot', function (data) {
     console.log(data);
   });
