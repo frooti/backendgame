d54641b91d8ad702dfbb5c24afa26c6882301daa
System Administrator <root@Muppalanenis-MacBook-Air.local>
2016-11-12 01:04:35 +0530

    added login

--- pusher/server.js
+++ pusher/server.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'satoshidigits-mysql.cbxoxdbgejfz.eu-west-1.rds.amazonaws.com',
+  user     : 'satoshidigits',
+  password : '$shubteja$',
+  database : 'satoshidigits'
+});
+
+connection.connect();
 
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
@@ -12,22 +22,34 @@ var options = {
 	'prefix': 'sess::',
 }
 
+// session middleware 
 app.use(session({
-    store: new RedisStore(options),
-    secret: '$at0shiN@kam0to'
+	name: 'sid',
+	store: new RedisStore(options),
+	secret: '$at0shiN@kam0to',
+	cookie: { secure: true, httpOnly: true}
 }));
 
 server.listen(8080, function(){
-  console.log('listening on *:8080');
+	console.log('listening on *:8080');
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
+});
+
+app.post('/login', function (req, res) {
+	var username = req.param('username');
+	var password = req.param('password');
+
+	connection.query('SELECT * FROM user', function(err, rows){
+    	res.render('users', {users : rows});
+  });
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+	console.log('user disconnected');
   });
 });
