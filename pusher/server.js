6cca51bbef6a89cbb6e6944752779e90ad16bbdc
ravi <ravi.muppalaneni@gmail.com>
2016-11-13 18:16:25 +0530

    adding client.

--- pusher/server.js
+++ pusher/server.js
@@ -59,7 +59,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 // static files
-app.use(express.static('static'));
+app.use(express.static(__dirname+'/static'));
 
 server.listen(8080, function(){
 	console.log('listening on *:8080');
